739131c91d8e2cdc8892899d998845387683bed8
giulio pascolat <Giuliopascolat@gmail.com>
2024-11-26 16:37:37 +0100

    restored original connection string

--- SqlClient/Program.cs
+++ SqlClient/Program.cs
@@ -1,7 +1,6 @@
 ﻿using SqlClient;
 
-//const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ToDoList;Integrated Security=True;TrustServerCertificate=True";
-const string connectionString = "Server=COMPUTER\\SQLEXPRESS;Database=ToDoList;TrustServerCertificate=True;Trusted_Connection=True;";
+const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ToDoList;Integrated Security=True;TrustServerCertificate=True";
 
 using var database = new Database(connectionString);
 using var unitOfWork = new UnitOfWork(database);
